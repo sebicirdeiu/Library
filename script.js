c10dfd47d1873cf197896b69360e2048362974da
Sebi Cirdeiu <sebbcirdeiu@yahoo.com>
2023-04-10 17:30:04 +0300

    Chanke constructor into class

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 let mylibrary = [];
 
-//Constructor pentru carte
+//definirea clasei
 
-function Book(title, author, pages, read) {
+class Book {
+constructor (title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-}
+}}
+
 
 
 function render() {
